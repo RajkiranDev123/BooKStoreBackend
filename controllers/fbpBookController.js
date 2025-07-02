775eb30d6d9f89bab558ebf70d58e7c22cfb0546
raj <rajtech645@gmail.com>
2025-07-02 20:05:30 +0530

    add book api modified

--- controllers/fbpBookController.js
+++ controllers/fbpBookController.js
@@ -6,7 +6,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 let getDir = process.cwd()
 
-//file based persistence
+//file based persistence (fbp)
+
+//create book
 
 export const fbpAddBook = catchAsyncErrors(async (req, res, next) => {
 
@@ -17,23 +19,19 @@ export const fbpAddBook = catchAsyncErrors(async (req, res, next) => {
         const id = uuidv4();
         const filePath = `${getDir}/jsonData/books.json`
         const data = await readFile(filePath, 'utf-8');
-        const json = JSON.parse(data);
-
-        json.books.push({ ...req.body, id });
-
-        await writeFile(filePath, JSON.stringify(json, null, 2), 'utf-8');
-
-
-        console.log("jsonData==>", json)
+        const allBooks = JSON.parse(data);
+        let bookObj = await BookModel.create({ ...req.body, id, userId: req.user._id })
+        allBooks.books.push(bookObj);
+        await writeFile(filePath, JSON.stringify(allBooks, null, 2), 'utf-8');
         return res.status(201).json({
-            success: true, message: "Book added !", book: { ...req.body, id }
+            success: true, message: "Book added !", bookObj
         })
     } catch (error) {
         return next(new ErrorHandler(error.message, 500))
     }
 })
 
-
+//retrieve all books
 export const fbpGetAllBooks = catchAsyncErrors(async (req, res, next) => {
 
     try {
@@ -48,13 +46,42 @@ export const fbpGetAllBooks = catchAsyncErrors(async (req, res, next) => {
     }
 })
 
-// delete
 
+//update book
+export const fbpUpdateBook = catchAsyncErrors(async (req, res, next) => {
+
+    try {
+        const { id } = req.params
+        const { title, author, description, price, quantity, genre, publishedYear } = req.body
+        if (!title || !author || !description || !price || !quantity || !genre || !publishedYear) return next(new ErrorHandler("All fields are required", 400))
+
+        const filePath = `${getDir}/jsonData/books.json`
+        const data = await readFile(filePath, 'utf-8');
+        const allBooks = JSON.parse(data);
+        const bookIndex = allBooks.books.findIndex(book => book.id === id);
+        if (bookIndex === -1) {
+            return next(new ErrorHandler("book id not available!", 400))
+
+        }
+        allBooks.books[bookIndex] = {
+            ...allBooks.books[bookIndex],
+            ...req.body
+        };
+        await writeFile(filePath, JSON.stringify(allBooks, null, 2), 'utf-8');
+        return res.status(201).json({
+            success: true, message: "book updated!", book: req.body
+        })
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
+
+//delete book
 export const fbpDeleteBook = catchAsyncErrors(async (req, res, next) => {
 
     try {
         const { id } = req.params
-      
+
         const filePath = `${getDir}/jsonData/books.json`
         const data = await readFile(filePath, 'utf-8');
         const allBooks = JSON.parse(data);
@@ -67,4 +94,7 @@ export const fbpDeleteBook = catchAsyncErrors(async (req, res, next) => {
         return next(new ErrorHandler(error.message, 500))
     }
 })
+
+
+
 
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -2,7 +2,7 @@ import { isAuthenticated, isAuthorized } from "../middlewares/authMiddleware.js"
 
 import { addBook, getAllBooks, deleteBook, getBookById, updateBookById } from "../controllers/bookController.js"
 
-import { fbpAddBook, fbpGetAllBooks ,fbpDeleteBook} from "../controllers/fbpBookController.js"
+import { fbpAddBook, fbpGetAllBooks, fbpDeleteBook } from "../controllers/fbpBookController.js"
 
 
 import express from "express"
@@ -10,15 +10,11 @@ import express from "express"
 
 const router = express.Router()
 
+// db based persistence
 router.post("/admin/add", isAuthenticated, isAuthorized("Admin"), addBook)
 router.get("/all", isAuthenticated, getAllBooks)
 router.get("/get-book/:id", isAuthenticated, getBookById)
-
-
-
 router.patch("/update-book/:id", isAuthenticated, isAuthorized("Admin"), updateBookById)
-
-
 router.delete("/delete/:id", isAuthenticated, isAuthorized("Admin"), deleteBook)
 
 //File-Based Persistence routes (fbp)
